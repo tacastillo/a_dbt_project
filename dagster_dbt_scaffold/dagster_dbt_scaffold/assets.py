93bdebc2efd5a354253979018438c59a8938cdda
Tim Castillo <timangcas@gmail.com>
2023-08-21 19:00:19 -0400

    Add Dagster project from `dagster-dbt project scaffold`

---
+++ dagster_dbt_scaffold/dagster_dbt_scaffold/assets.py
@@ -0,0 +1,10 @@
+from dagster import OpExecutionContext
+from dagster_dbt import DbtCliResource, dbt_assets
+
+from .constants import dbt_manifest_path
+
+
+@dbt_assets(manifest=dbt_manifest_path)
+def a_dbt_project_dbt_assets(context: OpExecutionContext, dbt: DbtCliResource):
+    yield from dbt.cli(["build"], context=context).stream()
+
---
+++ dagster_dbt_scaffold/dagster_dbt_scaffold/definitions.py
@@ -0,0 +1,17 @@
+import os
+
+from dagster import Definitions
+from dagster_dbt import DbtCliResource
+
+from .assets import a_dbt_project_dbt_assets
+from .constants import dbt_project_dir
+from .schedules import schedules
+
+defs = Definitions(
+    assets=[a_dbt_project_dbt_assets],
+    schedules=schedules,
+    resources={
+        "dbt": DbtCliResource(project_dir=os.fspath(dbt_project_dir)),
+    },
+)
+
