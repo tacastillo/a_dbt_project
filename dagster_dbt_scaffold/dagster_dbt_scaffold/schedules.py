c8ead8866a9c0ab4ec2a364782c16e36a997bde6
Tim Castillo <tim@dagsterlabs.com>
2023-08-24 16:27:23 -0400

    Update schedules.py

--- dagster_dbt_scaffold/dagster_dbt_scaffold/schedules.py
+++ dagster_dbt_scaffold/dagster_dbt_scaffold/schedules.py
@@ -6,11 +6,11 @@ from dagster_dbt import build_schedule_from_dbt_selection
 from .assets import a_dbt_project_dbt_assets
 
 schedules = [
-#     build_schedule_from_dbt_selection(
-#         [a_dbt_project_dbt_assets],
-#         job_name="materialize_dbt_models",
-#         cron_schedule="0 0 * * *",
-#         dbt_select="fqn:*",
-#     ),
+    build_schedule_from_dbt_selection(
+        [a_dbt_project_dbt_assets],
+        job_name="materialize_dbt_models",
+        cron_schedule="0 0 * * *",
+        dbt_select="fqn:*",
+    ),
 ]
 
